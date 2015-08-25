9042eb1310aeef95397c331c15ff006bb215d8f3
kevin <kevin@localhost.localdomain>
2015-08-25 17:13:08 +0100

    Update .travis.yml

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,13 +3,13 @@
 export PATH="$TRAVIS_BUILD_DIR/vendor/bin:$PATH"
 
 # Wipe out the DB
-drush psite-ewipe $PUUID $PENV -y
+#drush psite-ewipe $PUUID $PENV -y
 
 # Insure we are in SFTP mode for a clean install
-drush psite-cmode $PUUID $PENV sftp
+#drush psite-cmode $PUUID $PENV sftp
 
 # Install
-drush @pantheon.$PNAME.$PENV si --account-pass=$RANDPASS --site-name="Travis Did It" -y
+#drush @pantheon.$PNAME.$PENV si --account-pass=$RANDPASS --site-name="Travis Did It" -y
 
 # Run the barebones behat test
 behat
