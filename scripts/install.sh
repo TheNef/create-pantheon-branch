7beb8c7e4bb868b946145cc4c5bab152400366bc
kevin <kevin@localhost.localdomain>
2015-08-25 11:50:28 +0100

    First commit - force build

---
+++ scripts/install.sh
@@ -0,0 +1,13 @@
+ # Dynamic hosts through Pantheon mean constantly checking interactively
+  # that we mean to connect to an unknown host. We ignore those here.
+  - echo "StrictHostKeyChecking no" > ~/.ssh/config
+
+  # Install Drush.
+  - composer global require drush/drush:6.2.0
+  - export PATH="$HOME/.composer/vendor/bin:$PATH"
+
+  # Install Terminus.
+  - git clone https://github.com/pantheon-systems/terminus.git $HOME/.drush/terminus
+  - cd $HOME/.drush/terminus
+  - composer update --no-dev
+  - drush cc drush
